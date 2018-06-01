caca56c7bade91c7aa991ebcacdb1a1013ac4d94
toggor <toggor@gmail.com>
2018-06-01 21:46:46 +0300

    after linting

--- app.js
+++ app.js
@@ -3,17 +3,13 @@
 const genMatrix = require('./matrix');
 const runAlg = require('./algorithm');
 
-const arr = genMatrix(process.argv[2]);
-
+const arr = genMatrix(process.argv[2]); //  we get the matrix size from the commandline
 console.log('The matrix:');
-
 for (let i = 0; i <= arr.length - 1; i++) {
-    console.log(arr[i]);
+  console.log(arr[i]);
 }
-
 const result = runAlg(arr);
-
-console.log(`Resulting sequence: ${result.reverse()}`); // center is a last value and we need it to be first
+console.log(`Resulting sequence: ${result.reverse()}`); //  center is a last value and we need it to be first
 
 
 /*
--- matrix.js
+++ matrix.js
@@ -5,18 +5,18 @@
  * @returns {array} Returns the matrix in a form of square array
  */
 function genMatrix(inp) {
-    const n = parseInt(inp, 10);
-    if (!Number.isNaN(n) && n > 1) {
-        const arr = [];
-        for (let y = 0; y<2*n-1; y++){
-            arr[y] = [];
-            for (let x = 0; x<2*n-1; x++){
-                arr[y][x] = Math.floor(Math.random() * (9 - 0) + 0);
-            }
-        }
-        return arr;
+  const n = parseInt(inp, 10);
+  if (!Number.isNaN(n) && n > 1) {
+    const arr = [];
+    for (let y = 0; y < 2 * n - 1; y++) {
+      arr[y] = [];
+      for (let x = 0; x < 2 * n - 1; x++) {
+        arr[y][x] = Math.floor(Math.random() * (9 - 0) + 0);
+      }
     }
-    throw new Error('Invalid n. It must be positive integer and >1');
+    return arr;
+  }
+  throw new Error('Invalid n. It must be positive integer and >1');
 }
 
 module.exports = genMatrix;
